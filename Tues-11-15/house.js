7ade407a81afd67253731324b71f679d23e3a2d2
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-15 16:12:29 -0600

    committing work from Tuesday 11-14

---
+++ Tues-11-15/house.js
@@ -0,0 +1,32 @@
+var myHouse {
+
+  inside:{
+    bedrooms: 3,
+    bathrooms: 2.5,
+    livingSpaces: 2,
+
+      occupants: [
+      {
+      name: chelsey,
+      age: 28,
+      },
+      {
+      name: kevin,
+      age: 29,
+      },
+    ]
+  }
+  outside:{
+    areage: 2.5,
+    turf: buffaloGrass;
+  }
+
+    status:{
+    completionDate: march2017,
+    plumbing: nov2016,
+  }
+
+
+};
+console.log(myHouse)
+//objet literal syntax no spaces
