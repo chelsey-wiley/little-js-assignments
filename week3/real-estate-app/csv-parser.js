ed79981e37d477eff0262ae43ac37c39321327aa
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-22 10:10:11 -0600

    first portion of step-setting up the callback. Prior to actualy parsing of data.

---
+++ week3/real-estate-app/csv-parser.js
@@ -0,0 +1,43 @@
+var fs = require ('fs');
+
+
+
+
+function parse(callback) {
+  console.log('#3 - time to read the file')
+
+  var callbackForReadFile = function (err, fileContents){
+    console.log("#6 - some itme later, OS done reading Call back for fs.readFile.")
+  }
+    var output = {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log("#4 - delegated file reading on the OS - continue on.")
+
+}
--- week3/real-estate-app/real-estate.js
+++ week3/real-estate-app/real-estate.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
 
+var app = express();
+
 app.use(express.static('public'))
+
+app.get('/real-estate-data', function (req, res) {
+  console.log("#1 - request from browser received.");
+
+
+
+
+
+
+  console.log("#2 - time to call the parser");
+  parser(callback);
+  console.log("#5 - having delegated, I'm back in the handeler");
+});
+
+
+
+
+
+
+
 
 app.listen(4001, function() {
 });
